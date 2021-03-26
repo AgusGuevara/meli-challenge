1f0d50faaa74ba74ddc2ae062a83ec5efba800d0
Agustin Thomas Guevara <38946506+AgusGuevara@users.noreply.github.com>
2021-03-26 18:24:35 -0300

    Update app.js

--- backendmeli/app.js
+++ backendmeli/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = 8080;
 // routes
 const routes = require("./routes/routes");
 
